e48779945eab360295d79c9dbbba5cc5c45bb163
Jordan Asano <jordanasano@yahoo.com>
2022-05-26 14:19:34 -0700

    commented out makeBoard starter code

--- connect4.js
+++ connect4.js
@@ -58,7 +58,7 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *   board = array of rows, each row is array of cells  (board[y][x])
- */
+ 
 
 function makeBoard() {
   for (let y = 0; y < HEIGHT; y++) {
